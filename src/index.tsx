52ab5e51dd82fa7b65e4bbce1553555b67ef5004
Ami_Mari <ami.mari.art@gmail.com>
2026-01-21 22:59:04 +0100

    fix commitizen

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,9 @@
 
 // console.log(doubledNumbers); // 4, 6, 10
 
-
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/dog.jpg';
+import style from "./index.module.scss";
+import "./index.scss";
+import "./assets/images/dog.jpg";
 //import { ReactComponent as ReactLogo } from './assets/images/react.svg';
 
 console.log(style); //использование объекта style важно для попадания стилей в css-бандл
